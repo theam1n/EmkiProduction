699d3338470590721e244daef848bf0f1eeb270b
Amin Aliyev <aminaliyev670@gmail.com>
2024-08-14 15:59:40 +0400

    ee

---
+++ src/main/java/com/example/emkiproductionapplication/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.example.emkiproductionapplication.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final CorsConfigurationSource corsConfigurationSource;
+
+    public WebConfig(CorsConfigurationSource corsConfigurationSource) {
+        this.corsConfigurationSource = corsConfigurationSource;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
